66e13afac82fbe7f0ccc2b0c4a788515e9ae0e5f
Donnie D'Amato <fauxserious@gmail.com>
2020-03-08 11:05:21 -0400

    getter

--- feeds.js
+++ feeds.js
@@ -3,6 +3,7 @@ const FEED_API_URL_BASE = 'http://datamine.mta.info/mta_esi.php'; // ?key=KEY
 module.exports = [{
   feed_id: 1,
   lines: ['1', '2', '3', '4', '5', '6', 'S'],
+  get feed () { return `${FEED_API_URL_BASE}?feed_id=${this.feed_id}` }
 }, {
   feed_id: 26,
   lines: ['A', 'C', 'E'],
