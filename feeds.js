bb7ee23115c8c3c7032ac17389d7de1ca36fa313
Donnie D'Amato <fauxserious@gmail.com>
2020-03-08 09:58:13 -0400

    Add feeds

---
+++ feeds.js
@@ -0,0 +1,25 @@
+module.exports = [{
+  feed_id: 1,
+  lines: ['1', '2', '3', '4', '5', '6', 'S'],
+}, {
+  feed_id: 26,
+  lines: ['A', 'C', 'E'],
+}, {
+  feed_id: 16,
+  lines: ['N', 'Q', 'R', 'W'],
+}, {
+  feed_id: 21,
+  lines: ['B', 'D', 'F', 'M'],
+}, {
+  feed_id: 2,
+  lines: ['L'],
+}, {
+  feed_id: 31,
+  lines: ['G'],
+}, {
+  feed_id: 36,
+  lines: ['J', 'Z'],
+}, {
+  feed_id: 51,
+  lines: ['7'],
+}];
