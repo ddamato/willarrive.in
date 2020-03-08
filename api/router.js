7a45228d6babd6aebfab89b18602ff78ee0b15b3
Donnie D'Amato <fauxserious@gmail.com>
2020-03-08 11:21:26 -0400

    Add header

--- api/router.js
+++ api/router.js
@@ -1,7 +1,8 @@
 module.exports.handler = async (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hellow World' }),
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({ message: 'Hello World' }),
   };
   callback(response);
 }
