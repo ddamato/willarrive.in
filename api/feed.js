f11d33edef97c385d131e920cc1bd7ee5561e93e
Donnie D'Amato <fauxserious@gmail.com>
2020-03-08 11:36:15 -0400

    Get feed api

---
+++ api/feed.js
@@ -0,0 +1,8 @@
+module.exports.handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({ message: 'Hello World' }),
+  };
+  return response;
+}
