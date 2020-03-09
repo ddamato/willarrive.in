4364b7bcd0d0cb230e3919653612390e0b578999
Donnie D'Amato <fauxserious@gmail.com>
2020-03-09 16:04:04 -0400

    Capture domainPrefix

--- api/router.js
+++ api/router.js
@@ -4,9 +4,12 @@ const nunjucks = require('nunjucks');
 const TIMETABLE_NJK_PATH = path.resolve(__dirname, '..', 'templates', 'timetable.njk');
 
 module.exports.handler = (event, context, callback) => {
+  // domainPrefix.willarrive.in
+  const { domainPrefix } = event.requestContext || {};
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ event, context })
+    body: JSON.stringify({ domainPrefix })
   };
 
   // return nunjucks.render(TIMETABLE_NJK_PATH, { });
