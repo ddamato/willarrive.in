3be93216ad190fe77b23f71f5028f4bfede2a203
Donnie D'Amato <fauxserious@gmail.com>
2020-03-09 16:29:33 -0400

    Get feed through api

--- api/router.js
+++ api/router.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const nunjucks = require('nunjucks');
+const feeds = require('../feeds.js');
 
 const TIMETABLE_NJK_PATH = path.resolve(__dirname, '..', 'templates', 'timetable.njk');
 
 module.exports.handler = (event, context, callback) => {
+  const Location = 'https://willarrive.in';
+  let response = {
+    statusCode: 302,
+    headers: { Location }
+  }
+
   // domainPrefix.willarrive.in
   const { domainPrefix } = event.requestContext || {};
+  if (domainPrefix) {
+    const feed = feeds.find(({ lines }) => lines.includes(domainPrefix.toUpperCase()));
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ domainPrefix })
-  };
+    if (feed) {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({ feed }),
+      }
+    }
+  }
 
   // return nunjucks.render(TIMETABLE_NJK_PATH, { });
 
