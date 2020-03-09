f44b8ca6e88b70aedffaddecc3da876a2023ab2f
Donnie D'Amato <fauxserious@gmail.com>
2020-03-09 15:14:30 -0400

    Show event and context vars

--- api/router.js
+++ api/router.js
@@ -1,8 +1,10 @@
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const TIMETABLE_NJK_PATH = path.resolve(__dirname, '..', 'templates', 'timetable.njk');
+
 module.exports.handler = async (event, context) => {
-  const response = {
-    statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify({ message: 'Hello World' }),
-  };
-  return response;
+  return JSON.stringify({ event, context });
+
+  // return nunjucks.render(TIMETABLE_NJK_PATH, { });
 }
