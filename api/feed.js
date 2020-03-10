93245678e9837707b34ffb8af6ffbb271660fac0
Donnie D'Amato <fauxserious@gmail.com>
2020-03-10 10:23:44 -0400

    Get realtime from api

--- api/feed.js
+++ api/feed.js
@@ -1,7 +1,19 @@
 const prepareHeaders = require('../lib/headers.js');
+const { findFeed, findStationByCoords, findDeparturesByStation } = require('../lib/feed.js');
 
-module.exports.handler = (event, context, callback) => {
+module.exports.handler = async (event, context, callback) => {
   const headers = prepareHeaders(event);
+
+  const { queryStringParameters } = event;
+  const { latitude, longitude } = queryStringParameters || {};
+
+  if (latitude && longitude) {
+    const station = findStationByCoords({ latitude, longitude });
+    if (station) {
+      const departures = await findDeparturesByStation(station, event);
+    } 
+  }
+
   const response = {
     statusCode: 200,
     headers,
