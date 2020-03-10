e499154a10ef474b76633ba3138a87a2746e9601
Donnie D'Amato <fauxserious@gmail.com>
2020-03-10 18:31:59 -0400

    Update layout

--- public/script.js
+++ public/script.js
@@ -3,12 +3,22 @@ function getPosition(options) {
 }
 
 async function init() {
+  const allowPosition = document.querySelector('.allow-geo');
+  allowPosition.addEventListener('click', fetchFeed);  
+}
+
+async function fetchFeed() {
+  this.outerHTML = 'wait...';
+  const preGeo = document.querySelector('.pre-geo');
+  const postGeo = document.querySelector('.post-geo');
   try {
     const { coords } = await getPosition();
     const { name, schedule } = await fetch(`/feed?latitude=${coords.latitude}&longitude=${coords.longitude}`)
       .then((res) => res.json());
     directionSelection(schedule);
     atStation(name);
+    preGeo.classList.remove('active');
+    postGeo.classList.add('active');
   } catch (err) {
     throw new Error(err);
   }
@@ -29,25 +39,24 @@ function directionSelection(schedule) {
     options[option].push(entry.time);
   });
 
-  const select = document.createElement('select');
-  Object.keys(options).forEach((option) => {
-    const opt = document.createElement('option');
-    opt.textContent = option;
-    opt.value = option;
-    select.appendChild(opt);
-  });
-  document.body.appendChild(select);
-  select.addEventListener('change', onSelect);
+  const select = document.querySelector('.direction-selection');
+  select.innerHTML = Object.keys(options).map((option) => {
+    return `<option value=${option}>${option}</option>`;
+  }).join('');
+  const times = document.querySelector('.time-slider');
+  select.addEventListener('change', ({ target }) => onSelect(target, times));
 }
 
-function onSelect() {
-  console.log(options[this.value]);
+function onSelect(select, target) {
+  target.innerHTML = options[select.value].map((time) => {
+    // TODO: Convert to now diff
+    return `<li>${new Date(time).toLocaleString()}</li>`;
+  }).join('');
 }
 
 function atStation(name) {
-  const h1 = document.createElement('h1');
-  h1.textContent = ` at the ${name} station.`;
-  document.body.appendChild(h1);
+  const closestStation = document.querySelector('.closest-station');
+  closestStation = name;
 }
 
 if (window.location.href !== 'https://willarrive.in/') {
