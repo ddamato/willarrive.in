15859290469c521c469a7498cb084d78ede5e7cf
Donnie D'Amato <fauxserious@gmail.com>
2020-03-10 23:38:50 -0400

    Bugfixes

--- public/script.js
+++ public/script.js
@@ -4,14 +4,29 @@ function getPosition(options) {
 
 async function init() {
   const allowPosition = document.querySelector('.allow-geo');
-  allowPosition.addEventListener('click', fetchFeed);  
+  allowPosition.addEventListener('click', fetchFeed);
+}
+
+let position;
+
+async function fetchPosition() {
+  this.outerHTML = 'wait...';
+  try {
+    position = await getPosition();
+    fetchFeed();
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
 async function fetchFeed() {
-  this.outerHTML = 'wait...';
+  if (!position.coords) {
+    return;
+  }  
+
   try {
-    const { coords } = await getPosition();
-    const html = await fetch(`/feed?latitude=${coords.latitude}&longitude=${coords.longitude}`).then((res) => res.text());
+    const { latitude, longitude } = position.coords || {};
+    const html = await fetch(`/feed?latitude=${latitude}&longitude=${longitude}`).then((res) => res.text());
     handleResponse(html);
   } catch (err) {
     throw new Error(err);
@@ -22,17 +37,21 @@ function handleResponse(html) {
   document.body.innerHTML = html;
   const select = document.querySelector('.destinations');
   const time = document.querySelector('.time-delta');
+  const reloader = document.querySelector('.reload');
   select.addEventListener('change', () => {
     time.textContent = timeDiff(select.value);
   });
   time.textContent = timeDiff(select.value);
+  reloader.addEventListener('click', fetchFeed);
 }
 
 function timeDiff(isoTime) {
   const now = new Date().getTime();
   const future = new Date(isoTime).getTime();
-  const diff = Math.abs(future - now);
-  return `${Math.round(diff / 60000)} minutes`;
+  const minutes = Math.round((future - now) / 60000);
+  return Math.abs(minutes) === 1
+    ? `${minutes} minute`
+    : `${minutes} minutes`;
 }
 
 if (window.location.href !== 'https://willarrive.in/') {
