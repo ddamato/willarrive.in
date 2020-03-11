2cea5bcc7df24e875f3256c45345d4a8c44f4058
Donnie D'Amato <fauxserious@gmail.com>
2020-03-10 23:24:13 -0400

    Update styles

--- public/script.js
+++ public/script.js
@@ -20,6 +20,19 @@ async function fetchFeed() {
 
 function handleResponse(html) {
   document.body.innerHTML = html;
+  const select = document.querySelector('.destinations');
+  const time = document.querySelector('.time-delta');
+  select.addEventListener('change', () => {
+    time.textContent = timeDiff(select.value);
+  });
+  time.textContent = timeDiff(select.value);
+}
+
+function timeDiff(isoTime) {
+  const now = new Date().getTime();
+  const future = new Date(isoTime).getTime();
+  const diff = Math.abs(future - now);
+  return `${Math.round(diff / 60000)} minutes`;
 }
 
 if (window.location.href !== 'https://willarrive.in/') {
