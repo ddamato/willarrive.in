d1741193e79c6d4d6cccce30abe1a5bca42b4738
Donnie D'Amato <fauxserious@gmail.com>
2020-03-11 15:38:15 -0400

    Add station call

--- api/feed.js
+++ api/feed.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const nunjucks = require('nunjucks');
 const prepareHeaders = require('../lib/headers.js');
-const certainty = require('../lib/certainty.js');
+const certainties = require('../lib/certainty.js');
 const { 
   getLineByEvent,
   findNearestStop,
   parseArrivals,
 } = require('../lib/feed.js');
-const mta = require('../lib/mta.js');
+const {
+  getScheduleByLine,
+  getScheduleByStopId,
+} = require('../lib/mta.js');
 
 const FEED_NJK_PATH = path.resolve(__dirname, '..', 'templates', 'feed.njk');
 
@@ -21,20 +24,47 @@ module.exports.handler = async (event, context, callback) => {
   }
 
   const { queryStringParameters } = event;
-  const { latitude, longitude } = queryStringParameters || {};
+  const { latitude, longitude, station } = queryStringParameters || {};
+  const randomIndex = Math.floor(Math.random() * certainties.length);
+  const certainty = certainties[randomIndex];
+  const line = getLineByEvent(event);
 
   if (latitude && longitude) {
-    const randomIndex = Math.floor(Math.random() * certainty.length);
-    const line = getLineByEvent(event);
-    const stops = await mta(line);
-    const stop = findNearestStop(stops, Number(latitude), Number(longitude));
-    const arrivals = parseArrivals(stop);
+    const { stopName, arrivals } = await getArrivalsByCoords(line, latitude, longitude);
     response.body = nunjucks.render(FEED_NJK_PATH, {
       line,
-      certainty: certainty[randomIndex],
-      stopName: stop.name,
+      certainty, 
+      stopName,
       arrivals,
     });
   }
+
+  if (station) {
+    const { stopName, arrivals } = await getArrivalsByStation(station); 
+    response.body = nunjucks.render(FEED_NJK_PATH, {
+      line,
+      certainty,
+      stopName,
+      arrivals,
+    });
+  }
+
   callback(null, response);
+}
+
+async function getArrivalsByCoords(line, latitude, longitude) {
+  const stops = await getScheduleByLine(line);
+  const stop = findNearestStop(stops, Number(latitude), Number(longitude));
+  return {
+    stopName: stop.name,
+    arrivals: parseArrivals(stop),
+  }
+}
+
+async function getArrivalsByStation(station) {
+  const stop = getScheduleByStopId(station);
+  return {
+    stopName: stop.name,
+    arrivals: parseArrivals(stop),
+  }
 }
